f422ff088ef3bc67825b2803b159c02702a30bef
Ivantseng123 <as82858285@gmail.com>
2024-10-04 00:26:24 +0800

    新增連線azure database的設定

--- src/main/java/com/ProFit/ProFitApplication.java
+++ src/main/java/com/ProFit/ProFitApplication.java
@@ -2,8 +2,10 @@ package com.ProFit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:db-secrets.properties")
 public class ProFitApplication {
 
 	public static void main(String[] args) {
