02a4be93c33c924649e7d5865ce619f1a76bc6fd
Ivantseng123 <as82858285@gmail.com>
2024-10-08 02:34:54 +0800

    完成courseLesson的搜尋全部功能

--- src/main/java/com/ProFit/controller/courses/CourseLessonsController.java
+++ src/main/java/com/ProFit/controller/courses/CourseLessonsController.java
@@ -1,10 +1,38 @@
 package com.ProFit.controller.courses;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ProFit.model.dto.coursesDTO.CourseLessonDTO;
+import com.ProFit.model.dto.coursesDTO.CourseModuleDTO;
+import com.ProFit.service.courseService.IcourseLessonService;
+import com.ProFit.service.courseService.IcourseModuleService;
 
 @Controller
 public class CourseLessonsController {
 
+	@Autowired
+	private IcourseLessonService courseLessonService;
 	
+	@Autowired
+	private IcourseModuleService courseModuleService;
+	
+	@GetMapping("/courseLessons")
+	public String courseLessonsPage(@RequestParam Integer courseModuleId,Model model) {
+		
+		List<CourseLessonDTO> courseLessonDTOList = courseLessonService.searchCourseLessons(courseModuleId);
+		
+		CourseModuleDTO courseModuleDTO = courseModuleService.searchOneCourseModuleById(courseModuleId);
+		
+		model.addAttribute("courseModuleDTO",courseModuleDTO);
+		model.addAttribute("courseLessonDTOList",courseLessonDTOList);
+		
+		return "coursesVIEW/courseLessonView";
+	}
 	
 }
