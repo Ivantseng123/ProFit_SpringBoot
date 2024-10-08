7ec4d33f0580cb16c53605b89da4f869faa3d062
DEVONHUI <p8310112000@gmail.com>
2024-10-08 21:52:52 +0800

    修改sql語句設定（暫不影響）

--- ProFitDataBase創表語句.sql
+++ ProFitDataBase創表語句.sql
@@ -67,7 +67,7 @@ CREATE TABLE jobs (
     jobs_id INT PRIMARY KEY IDENTITY(1,1), 
     jobs_user_id INT,
     jobs_title NVARCHAR(100), 
-    jobs_posting_date DATETIME, 
+    jobs_posting_date DATETIME default Getdate(),
     jobs_application_deadline DATETIME, 
     jobs_description NVARCHAR(MAX), 
     jobs_status TINYINT, 
@@ -80,6 +80,17 @@ CREATE TABLE jobs (
     FOREIGN KEY (jobs_user_id) REFERENCES users(user_id)
 );
 
+-- CREATE TABLE [dbo].[jobs_application] (
+--     [jobs_application_id]         INT             IDENTITY (1, 1) NOT NULL,
+--     [jobs_application_posting_id] INT             NOT NULL,
+--     [jobs_application_member_id]  INT             NOT NULL,
+--     [jobs_application_date]       DATE            CONSTRAINT [DEFAULT_jobs_application_jobs_application_date] DEFAULT (getdate()) NULL,
+--     [jobs_application_status]     TINYINT         DEFAULT ((0)) NULL,
+--     [jobs_application_contract]   VARBINARY (MAX) NULL,
+--     PRIMARY KEY CLUSTERED ([jobs_application_id] ASC),
+--     CONSTRAINT [FK_jobs_application_member_id] FOREIGN KEY ([jobs_application_member_id]) REFERENCES [dbo].[users] ([user_id]),
+--     CONSTRAINT [FK_jobs_application_posting_id] FOREIGN KEY ([jobs_application_posting_id]) REFERENCES [dbo].[users] ([user_id])
+-- );
 CREATE TABLE [dbo].[jobs_application] (
     [jobs_application_id]         INT             IDENTITY (1, 1) NOT NULL,
     [jobs_application_posting_id] INT             NOT NULL,
@@ -90,7 +101,10 @@ CREATE TABLE [dbo].[jobs_application] (
     PRIMARY KEY CLUSTERED ([jobs_application_id] ASC),
     CONSTRAINT [FK_jobs_application_member_id] FOREIGN KEY ([jobs_application_member_id]) REFERENCES [dbo].[users] ([user_id]),
     CONSTRAINT [FK_jobs_application_posting_id] FOREIGN KEY ([jobs_application_posting_id]) REFERENCES [dbo].[users] ([user_id])
-);
+    );
+
+
+
 
 CREATE TABLE jobs_application_project (
     jobs_application_project_id INT PRIMARY KEY IDENTITY(1,1),
