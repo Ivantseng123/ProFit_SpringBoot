b55b7dd5c79a2841283a1d48f91567b4d9ef2bee
Ivantseng123 <as82858285@gmail.com>
2024-10-20 12:40:02 +0800

    在MajorCategory增加OneToMany CourseBean，並完成前端生成類別

--- src/main/java/com/ProFit/model/dto/coursesDTO/CourseCategoryDTO.java
+++ src/main/java/com/ProFit/model/dto/coursesDTO/CourseCategoryDTO.java
@@ -3,29 +3,43 @@ package com.ProFit.model.dto.coursesDTO;
 import com.ProFit.model.bean.majorsBean.MajorCategoryBean;
 
 public class CourseCategoryDTO {
-	
+
 	private String categoryName;
 	private Integer majorCategoryId;
-	
-	
+	private Integer courseNumber;
+
 	public CourseCategoryDTO() {
-		
+
 	}
+
 	public CourseCategoryDTO(MajorCategoryBean majorCategoryBean) {
 		this.majorCategoryId = majorCategoryBean.getMajorCategoryId();
 		this.categoryName = majorCategoryBean.getCategoryName();
+		this.courseNumber = majorCategoryBean.getCourseList().size();
 	}
+
 	public String getCategoryName() {
 		return categoryName;
 	}
+
 	public void setCategoryName(String categoryName) {
 		this.categoryName = categoryName;
 	}
+
 	public Integer getMajorCategoryId() {
 		return majorCategoryId;
 	}
+
 	public void setMajorCategoryId(Integer majorCategoryId) {
 		this.majorCategoryId = majorCategoryId;
 	}
-	
+
+	public Integer getCourseNumber() {
+		return courseNumber;
+	}
+
+	public void setCourseNumber(Integer courseNumber) {
+		this.courseNumber = courseNumber;
+	}
+
 }
