5dc1c91e8ae103e61ffb19bd2f466ea09f1efa20
DEVONHUI <p8310112000@gmail.com>
2024-10-23 00:07:01 +0800

    取用家任的major資料，在MajorBean增加List<Jobs> 新增多對多關聯表jobs_major

--- src/main/java/com/ProFit/model/bean/jobsBean/Jobs.java
+++ src/main/java/com/ProFit/model/bean/jobsBean/Jobs.java
@@ -1,35 +1,33 @@
 package com.ProFit.model.bean.jobsBean;
 
 
+import com.ProFit.model.bean.majorsBean.MajorBean;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.ProFit.model.bean.usersBean.Users;
 import org.springframework.format.annotation.DateTimeFormat;
 
 
 @Entity
 @Table(name = "jobs")
-public class Jobs implements java.io.Serializable{
-	private static final long serialVersionUID = 1L;//序列化，就像是身分證
+public class Jobs implements java.io.Serializable {
+    private static final long serialVersionUID = 1L;//序列化，就像是身分證
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "jobs_id")
     private Integer jobsId;
-    
+
     @ManyToOne(fetch = FetchType.EAGER)//FK，對User表，一個公司（user）可以有po很多職缺
-    @JoinColumn(name= "jobs_user_id")
+    @JoinColumn(name = "jobs_user_id")
     private Users users;//請跟我有關聯的表格組員修改他們的關係語句
 
 //    @ManyToOne(fetch = FetchType.LAZY)//改FK，對major表，一個技能可以對應很多職缺
@@ -38,22 +36,19 @@ public class Jobs implements java.io.Serializable{
 //    
 //    @Column(name = "jobs_user_id")
 //    private Integer jobsUserId;
-    
-    @Column(name = "jobs_required_skills")
-    private String jobsRequiredSkills;
 
     @Column(name = "jobs_title")
     private String jobsTitle;
 
     @Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	@JsonFormat(pattern = "yyyy-MM-dd")
-    @Column(name = "jobs_posting_date",insertable = false ,updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "jobs_posting_date", insertable = false, updatable = false)
     private Date jobsPostingDate;
 
     @Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	@JsonFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     @Column(name = "jobs_application_deadline")
     private Date jobsApplicationDeadline;
 
@@ -78,242 +73,162 @@ public class Jobs implements java.io.Serializable{
     @Column(name = "jobs_number_of_openings")
     private Integer jobsNumberOfOpenings;
 
+    @ManyToMany
+    @JoinTable(name = "jobs_major", joinColumns = @JoinColumn(name = "jobs_id"),
+            inverseJoinColumns = @JoinColumn(name = "major_id"))
+    private List<MajorBean> majors = new ArrayList<>();
 
-    
-    
-    
-	public Jobs() {
-		super();
-			}
+    @OneToMany(mappedBy = "jobs", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<JobsApplication> applications = new ArrayList<>();
 
+    public Jobs() {
+        super();
+    }
 
 
+    public Integer getJobsId() {
+        return jobsId;
+    }
 
+    public void setJobsId(Integer jobsId) {
+        this.jobsId = jobsId;
+    }
 
-	public Integer getJobsId() {
-		return jobsId;
-	}
 
+    public Users getUsers() {
+        return users;
+    }
 
 
+    public void setUsers(Users users) {
+        this.users = users;
+    }
 
 
-	public void setJobsId(Integer jobsId) {
-		this.jobsId = jobsId;
-	}
+    public String getJobsTitle() {
+        return jobsTitle;
+    }
 
 
+    public void setJobsTitle(String jobsTitle) {
+        this.jobsTitle = jobsTitle;
+    }
 
 
+    public Date getJobsPostingDate() {
+        return jobsPostingDate;
+    }
 
-	public Users getUsers() {
-		return users;
-	}
 
+    public void setJobsPostingDate(Date jobsPostingDate) {
+        this.jobsPostingDate = jobsPostingDate;
+    }
 
 
+    public Date getJobsApplicationDeadline() {
+        return jobsApplicationDeadline;
+    }
 
 
-	public void setUsers(Users users) {
-		this.users = users;
-	}
+    public void setJobsApplicationDeadline(Date jobsApplicationDeadline) {
+        this.jobsApplicationDeadline = jobsApplicationDeadline;
+    }
 
 
+    public String getJobsDescription() {
+        return jobsDescription;
+    }
 
 
+    public void setJobsDescription(String jobsDescription) {
+        this.jobsDescription = jobsDescription;
+    }
 
-	public String getJobsTitle() {
-		return jobsTitle;
-	}
 
+    public String getJobsLocation() {
+        return jobsLocation;
+    }
 
 
+    public void setJobsLocation(String jobsLocation) {
+        this.jobsLocation = jobsLocation;
+    }
 
 
-	public void setJobsTitle(String jobsTitle) {
-		this.jobsTitle = jobsTitle;
-	}
+    public Byte getJobsStatus() {
+        return jobsStatus;
+    }
 
 
+    public void setJobsStatus(Byte jobsStatus) {
+        this.jobsStatus = jobsStatus;
+    }
 
 
+    public Integer getJobsMaxSalary() {
+        return jobsMaxSalary;
+    }
 
-	public Date getJobsPostingDate() {
-		return jobsPostingDate;
-	}
 
+    public void setJobsMaxSalary(Integer jobsMaxSalary) {
+        this.jobsMaxSalary = jobsMaxSalary;
+    }
 
 
+    public Integer getJobsMinSalary() {
+        return jobsMinSalary;
+    }
 
 
-	public void setJobsPostingDate(Date jobsPostingDate) {
-		this.jobsPostingDate = jobsPostingDate;
-	}
+    public void setJobsMinSalary(Integer jobsMinSalary) {
+        this.jobsMinSalary = jobsMinSalary;
+    }
 
 
+    public String getJobsWorktime() {
+        return jobsWorktime;
+    }
 
 
+    public void setJobsWorktime(String jobsWorktime) {
+        this.jobsWorktime = jobsWorktime;
+    }
 
-	public Date getJobsApplicationDeadline() {
-		return jobsApplicationDeadline;
-	}
 
+    public Integer getJobsNumberOfOpenings() {
+        return jobsNumberOfOpenings;
+    }
 
 
+    public void setJobsNumberOfOpenings(Integer jobsNumberOfOpenings) {
+        this.jobsNumberOfOpenings = jobsNumberOfOpenings;
+    }
 
 
-	public void setJobsApplicationDeadline(Date jobsApplicationDeadline) {
-		this.jobsApplicationDeadline = jobsApplicationDeadline;
-	}
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
 
 
+    public List<MajorBean> getMajors() {
+        return majors;
+    }
 
+    public void setMajors(List<MajorBean> majors) {
+        this.majors = majors;
+    }
 
+    public List<JobsApplication> getApplications() {
+        return applications;
+    }
 
-	public String getJobsDescription() {
-		return jobsDescription;
-	}
+    public void setApplications(List<JobsApplication> applications) {
+        this.applications = applications;
+    }
 
-
-
-
-
-	public void setJobsDescription(String jobsDescription) {
-		this.jobsDescription = jobsDescription;
-	}
-
-
-
-
-
-	public String getJobsLocation() {
-		return jobsLocation;
-	}
-
-
-
-
-
-	public void setJobsLocation(String jobsLocation) {
-		this.jobsLocation = jobsLocation;
-	}
-
-
-
-
-
-	public String getJobsRequiredSkills() {
-		return jobsRequiredSkills;
-	}
-
-
-
-
-
-	public void setJobsRequiredSkills(String jobsRequiredSkills) {
-		this.jobsRequiredSkills = jobsRequiredSkills;
-	}
-
-
-
-
-
-	public Byte getJobsStatus() {
-		return jobsStatus;
-	}
-
-
-
-
-
-	public void setJobsStatus(Byte jobsStatus) {
-		this.jobsStatus = jobsStatus;
-	}
-
-
-
-
-
-	public Integer getJobsMaxSalary() {
-		return jobsMaxSalary;
-	}
-
-
-
-
-
-	public void setJobsMaxSalary(Integer jobsMaxSalary) {
-		this.jobsMaxSalary = jobsMaxSalary;
-	}
-
-
-
-
-
-	public Integer getJobsMinSalary() {
-		return jobsMinSalary;
-	}
-
-
-
-
-
-	public void setJobsMinSalary(Integer jobsMinSalary) {
-		this.jobsMinSalary = jobsMinSalary;
-	}
-
-
-
-
-
-	public String getJobsWorktime() {
-		return jobsWorktime;
-	}
-
-
-
-
-
-	public void setJobsWorktime(String jobsWorktime) {
-		this.jobsWorktime = jobsWorktime;
-	}
-
-
-
-
-
-	public Integer getJobsNumberOfOpenings() {
-		return jobsNumberOfOpenings;
-	}
-
-
-
-
-
-	public void setJobsNumberOfOpenings(Integer jobsNumberOfOpenings) {
-		this.jobsNumberOfOpenings = jobsNumberOfOpenings;
-	}
-
-
-
-
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
-
-
-
-
-	@Override
-	public String toString() {
-		return "Jobs [jobsId=" + jobsId + ", users=" + users + ", jobsRequiredSkills=" + jobsRequiredSkills
-				+ ", jobsTitle=" + jobsTitle + ", jobsPostingDate=" + jobsPostingDate + ", jobsApplicationDeadline="
-				+ jobsApplicationDeadline + ", jobsDescription=" + jobsDescription + ", jobsLocation=" + jobsLocation
-				+ ", jobsStatus=" + jobsStatus + ", jobsMaxSalary=" + jobsMaxSalary + ", jobsMinSalary=" + jobsMinSalary
-				+ ", jobsWorktime=" + jobsWorktime + ", jobsNumberOfOpenings=" + jobsNumberOfOpenings + "]";
-	}
-   
-
+    @Transient
+    @JsonIgnore
+    public List<Integer> getMajorIds() {
+        return getMajors().stream().map(MajorBean::getMajorId).collect(Collectors.toList());
+    }
 }
