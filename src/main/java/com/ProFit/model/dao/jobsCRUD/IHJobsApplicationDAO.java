3586543e174f7ca8e2f0127450daaefe709bf041
DEVONHUI <p8310112000@gmail.com>
2024-10-24 23:20:59 +0800

    前台完成jobslist.jobsDetail 給峻翰方法 創表語句修正

--- src/main/java/com/ProFit/model/dao/jobsCRUD/IHJobsApplicationDAO.java
+++ src/main/java/com/ProFit/model/dao/jobsCRUD/IHJobsApplicationDAO.java
@@ -2,10 +2,14 @@ package com.ProFit.model.dao.jobsCRUD;
 
 
 import com.ProFit.model.bean.jobsBean.JobsApplication;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
-public interface IHJobsApplicationDAO extends CrudRepository<JobsApplication,Integer > {
+public interface IHJobsApplicationDAO extends JpaRepository<JobsApplication,Integer > {
+
 }
---
+++ src/main/java/com/ProFit/model/dto/jobsDTO/PageDTO.java
@@ -0,0 +1,22 @@
+package com.ProFit.model.dto.jobsDTO;
+
+public class PageDTO {
+    private Integer totalPage;
+    private Integer currentPage;
+
+    public Integer getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(Integer totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public Integer getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(Integer currentPage) {
+        this.currentPage = currentPage;
+    }
+}
