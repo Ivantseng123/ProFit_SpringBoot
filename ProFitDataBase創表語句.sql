5839913bc461e8bbe6431d8875db5c75165c9a7c
DEVONHUI <p8310112000@gmail.com>
2024-10-26 02:12:17 +0800

    增修欄位

--- ProFitDataBase創表語句.sql
+++ ProFitDataBase創表語句.sql
@@ -83,12 +83,12 @@ CREATE TABLE jobs (
 
 
 CREATE TABLE [dbo].[jobs_application] (
-    [jobs_application_id]        INT             IDENTITY (1, 1) NOT NULL,
-    [jobs_application_jobs_id]   INT             NOT NULL,
-    [jobs_application_member_id] INT             NOT NULL,
-    [jobs_application_date]      DATETIME2 (7)   CONSTRAINT [DEFAULT_jobs_application_jobs_application_date] DEFAULT (dateadd(hour,(8),getdate())) NULL,
-    [jobs_application_status]    TINYINT         CONSTRAINT [DF_jobs_application_status] DEFAULT ((0)) NULL,
-    [jobs_application_contract]  varchar(MAX) NULL,
+    [jobs_application_id]        INT           IDENTITY (1, 1) NOT NULL,
+    [jobs_application_jobs_id]   INT           NOT NULL,
+    [jobs_application_member_id] INT           NOT NULL,
+    [jobs_application_date]      DATETIME2 (7) CONSTRAINT [DEFAULT_jobs_application_jobs_application_date] DEFAULT (dateadd(hour,(8),getdate())) NULL,
+    [jobs_application_status]    TINYINT       CONSTRAINT [DF_jobs_application_status] DEFAULT ((0)) NULL,
+    [jobs_application_resume]    VARCHAR (MAX) NULL,
     PRIMARY KEY CLUSTERED ([jobs_application_id] ASC),
     FOREIGN KEY ([jobs_application_member_id]) REFERENCES [dbo].[users] ([user_id]),
     CONSTRAINT [FK_jobs_application_jobs_id] FOREIGN KEY ([jobs_application_jobs_id]) REFERENCES [dbo].[users] ([user_id])
@@ -101,16 +101,22 @@ ALTER TABLE [dbo].[jobs_application] NOCHECK CONSTRAINT [FK_jobs_application_job
 
 
 
-CREATE TABLE jobs_application_project (
-    jobs_application_project_id INT PRIMARY KEY IDENTITY(1,1),
-    jobs_application_id INT,
-    jobs_application_status TINYINT,
-    jobs_project VARCHAR(50),
-    jobs_amount INT,
-    FOREIGN KEY (jobs_application_id) REFERENCES jobs_application(jobs_application_id) 
-);
 
-    -- 專業技能的分類, 一個技能只能屬於一種分類
+
+CREATE TABLE [dbo].[jobs_application_project] (
+    [jobs_application_project_id] INT           IDENTITY (1, 1) NOT NULL,
+    [jobs_application_id]         INT           NULL,
+    [jobs_application_status]     TINYINT       NULL,
+    [jobs_project]                VARCHAR (50)  NULL,
+    [jobs_amount]                 INT           NULL,
+    [jobs_contract]               VARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([jobs_application_project_id] ASC),
+    FOREIGN KEY ([jobs_application_id]) REFERENCES [dbo].[jobs_application] ([jobs_application_id])
+    );
+
+
+
+-- 專業技能的分類, 一個技能只能屬於一種分類
 	CREATE TABLE major_category (
 	major_category_id INT primary key,
 	category_name NVARCHAR(24) not null,
--- src/main/java/com/ProFit/model/bean/jobsBean/JobsApplicationProject.java
+++ src/main/java/com/ProFit/model/bean/jobsBean/JobsApplicationProject.java
@@ -25,107 +25,92 @@ import jakarta.persistence.Table;
 public class JobsApplicationProject {
 
 
-
-	    @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    @Column(name = "jobs_application_project_id")
-	    private int jobsApplicationProjectId;
-
-
-	    @ManyToOne(fetch = FetchType.LAZY)//FK,一個申請對應到一個申請項目
-	    // cascade = CascadeType.ALL會同步更新刪除
-	    @JoinColumn(name = "jobs_application_id")
-	    private JobsApplication jobsApplication;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "jobs_application_project_id")
+    private int jobsApplicationProjectId;
 
 
-	    //一個狀態對應到一個申請項目
-	    @Column(name = "jobs_application_status")
-	    private Byte jobsApplicationStatus = 0;
-
-	    @Column(name = "jobs_project")
-	    private String jobsProject;
-
-	    @Column(name = "jobs_amount")
-	    private Integer jobsAmount;
+    @ManyToOne(fetch = FetchType.LAZY)//FK,一個申請對應到一個申請項目
+    // cascade = CascadeType.ALL會同步更新刪除
+    @JoinColumn(name = "jobs_application_id")
+    private JobsApplication jobsApplication;
 
 
+    //一個狀態對應到一個申請項目
+    @Column(name = "jobs_application_status")
+    private Byte jobsApplicationStatus = 0;
+
+    @Column(name = "jobs_project")
+    private String jobsProject;
+
+    @Column(name = "jobs_amount")
+    private Integer jobsAmount;
+
+    @Column(name = "jobs_contract")
+    private Integer jobsContract;
 
 
-	    public JobsApplicationProject() {
-			super();
-		}
+    public JobsApplicationProject() {
+        super();
+    }
 
 
+    public int getJobsApplicationProjectId() {
+        return jobsApplicationProjectId;
+    }
 
 
-		public int getJobsApplicationProjectId() {
-			return jobsApplicationProjectId;
-		}
+    public void setJobsApplicationProjectId(int jobsApplicationProjectId) {
+        this.jobsApplicationProjectId = jobsApplicationProjectId;
+    }
 
 
+    public JobsApplication getJobsApplication() {
+        return jobsApplication;
+    }
 
 
-		public void setJobsApplicationProjectId(int jobsApplicationProjectId) {
-			this.jobsApplicationProjectId = jobsApplicationProjectId;
-		}
+    public void setJobsApplication(JobsApplication jobsApplication) {
+        this.jobsApplication = jobsApplication;
+
+    }
 
 
+    public Byte getJobsApplicationStatus() {
+        return jobsApplicationStatus;
+    }
 
 
-		public JobsApplication getJobsApplication() {
-			return jobsApplication;
-		}
+    public void setJobsApplicationStatus(Byte jobsApplicationStatus) {
+        this.jobsApplicationStatus = jobsApplicationStatus;
+    }
 
 
+    public String getJobsProject() {
+        return jobsProject;
+    }
 
 
-		public void setJobsApplication(JobsApplication jobsApplication) {
-			this.jobsApplication = jobsApplication;
-
-		}
-
+    public void setJobsProject(String jobsProject) {
+        this.jobsProject = jobsProject;
+    }
 
 
-
-		public Byte getJobsApplicationStatus() {
-			return jobsApplicationStatus;
-		}
-
+    public Integer getJobsAmount() {
+        return jobsAmount;
+    }
 
 
+    public void setJobsAmount(Integer jobsAmount) {
+        this.jobsAmount = jobsAmount;
+    }
 
-		public void setJobsApplicationStatus(Byte jobsApplicationStatus) {
-			this.jobsApplicationStatus = jobsApplicationStatus;
-		}
+    public Integer getJobsContract() {
+        return jobsContract;
+    }
 
-
-
-
-		public String getJobsProject() {
-			return jobsProject;
-		}
-
-
-
-
-		public void setJobsProject(String jobsProject) {
-			this.jobsProject = jobsProject;
-		}
-
-
-
-
-		public Integer getJobsAmount() {
-			return jobsAmount;
-		}
-
-
-
-
-		public void setJobsAmount(Integer jobsAmount) {
-			this.jobsAmount = jobsAmount;
-		}
-
-
-
+    public void setJobsContract(Integer jobsContract) {
+        this.jobsContract = jobsContract;
+    }
 }
