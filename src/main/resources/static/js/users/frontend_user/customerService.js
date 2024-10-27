030e2683c144b6c43864b0e22dcdea467f401639
John <johnyu91308@gmail.com>
2024-10-27 23:53:14 +0800

    修改帳戶中心頁面

--- src/main/resources/static/js/users/frontend_user/customerService.js
+++ src/main/resources/static/js/users/frontend_user/customerService.js
@@ -1,37 +1,3 @@
-$(document).ready(function() {
-
-	getUser();
-
-});
-
-
-function getUser() {
-
-
-	if (localStorage.getItem('isLoggedIn') == null) {
-		alert("請先登入");
-		window.location.href = "http://localhost:8080/ProFit/homepage";
-	}
-	let sender = document.getElementById('sender')
-
-	fetch('http://localhost:8080/ProFit/user/profileinfo')
-		.then(response => {
-			if (response.ok) {
-				//throw new Error('Network response was not ok');
-				console.log("成功取得會員資料");
-			}
-			return response.json();
-
-		})
-		.then(user => {
-
-			sender.value = user.userEmail;
-
-
-		})
-		.catch(error => console.error('Error fetching user data:', error));
-}
-
 document.getElementById('questionForm').addEventListener('submit', function(e) {
 
 	e.preventDefault(); // 取消原本 form 表單送的 request
