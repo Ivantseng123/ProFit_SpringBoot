8a6473d544b89f0918de3ac170e234afa7cddb43
Daniel <danielkevien@gmail.com>
2024-10-28 18:13:56 +0800

    建立與編輯委託前端完成

--- src/main/java/com/ProFit/model/dao/chatsCRUD/ChatRepository.java
+++ src/main/java/com/ProFit/model/dao/chatsCRUD/ChatRepository.java
@@ -1,10 +1,41 @@
 package com.ProFit.model.dao.chatsCRUD;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ProFit.model.bean.chatsBean.ChatBean;
+import com.ProFit.model.bean.usersBean.Users;
+import com.ProFit.model.dto.chatsDTO.ChatUserDTO;
 
 public interface ChatRepository extends JpaRepository<ChatBean, Integer> {
-  
+
   ChatBean findByUserId1AndUserId2AndServiceId(Integer userId1, Integer userId2, Integer serviceId);
+
+  // 查 案主 有的聊天室
+  Page<ChatBean> findByUserId2(Integer userId2, Pageable pageable);
+
+  // 查 接案客 有的聊天室
+  Page<ChatBean> findByUserId1(Integer userId1, Pageable pageable);
+
+  // 查 跟案主 有聊天室的 接案客
+  @Query("""
+      SELECT new com.ProFit.model.dto.chatsDTO.ChatUserDTO(
+            u.id AS userId,
+            u.userName,
+            u.userEmail,
+            u.userPictureURL,
+            MAX(c.lastMessageAt)
+        )
+         FROM ChatBean c
+         JOIN Users u ON c.userId1 = u.id
+         WHERE c.userId2 = :userId2
+         GROUP BY u.id, u.userName, u.userEmail, u.userPictureURL
+        ORDER BY MAX(c.lastMessageAt) DESC
+      """)
+  List<ChatUserDTO> findUsersByUserId2(Integer userId2);
+
 }
