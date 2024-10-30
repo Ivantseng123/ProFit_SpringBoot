361d735dc41d14094df73c02dfeb5805ba0b99f4
Ivantseng123 <as82858285@gmail.com>
2024-10-30 15:57:50 +0800

    微調course功能

--- src/main/resources/static/js/courses/courseModuleView.js
+++ src/main/resources/static/js/courses/courseModuleView.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	console.log("Document is ready");
 	let params = new URLSearchParams(window.location.search);
 	let courseId = params.get('courseId');
@@ -6,7 +6,7 @@ $(document).ready(function() {
 	console.log(courseId);
 
 	// 當新增章節按鈕被按下時
-	$('#createBtn').click(function() {
+	$('#createBtn').click(function () {
 		let lastIndex = $('#table-body tr').length;
 		let newIndex = lastIndex + 1;
 
@@ -25,7 +25,7 @@ $(document).ready(function() {
 	});
 
 	// 確定新增
-	$(document).on('click', '.save', function() {
+	$(document).on('click', '.save', function () {
 		let moduleName = $('#newModuleName').val();
 
 		if (moduleName.trim() === "") {
@@ -37,7 +37,7 @@ $(document).ready(function() {
 			url: contextPath + '/courseModules/addModule',
 			data: { courseId: courseId, courseModuleName: moduleName },
 			type: 'POST',
-			success: function(response) {
+			success: function (response) {
 				if (response) {
 					window.alert('章節新增成功');
 					window.location.href = contextPath + '/courseModules?courseId=' + courseId;
@@ -45,7 +45,7 @@ $(document).ready(function() {
 					window.alert('新增失敗');
 				}
 			},
-			error: function(error) {
+			error: function (error) {
 				console.error('Error adding courseModule:', error);
 			}
 		});
@@ -58,12 +58,12 @@ $(document).ready(function() {
 	});
 
 	// 取消新增
-	$(document).on('click', '.cancel', function() {
+	$(document).on('click', '.cancel', function () {
 		$(this).closest('tr').remove();
 	});
 
 	// 點擊編輯按鈕
-	$(document).on('click', '.edit', function() {
+	$(document).on('click', '.edit', function () {
 		let $row = $(this).closest('tr');
 		let currentModuleName = $row.find('td:eq(1)').text().trim();
 		// 將章節名稱變成輸入框，並顯示保存和取消按鈕
@@ -75,47 +75,47 @@ $(document).ready(function() {
 	});
 
 	// 點擊保存編輯按鈕
-	$(document).on('click', '.save-edit', function() {
-	    let $row = $(this).closest('tr');
-	    let newModuleName = $row.find('.editModuleName').val().trim();
-	    let courseModuleId = $row.find('.sort').data('coursemoduleid');  // 從當前行的 .edit 按鈕上抓取 data-courseModuleId
-	
-		console.log(courseModuleId);
-		
-	    if (newModuleName === "") {
-	        alert("章節名稱不能為空");
-	        return;
-	    }
+	$(document).on('click', '.save-edit', function () {
+		let $row = $(this).closest('tr');
+		let newModuleName = $row.find('.editModuleName').val().trim();
+		let courseModuleId = $row.find('.sort').data('coursemoduleid');  // 從當前行的 .edit 按鈕上抓取 data-courseModuleId
 
-	    // 發送 Ajax 請求更新章節名稱
-	    $.ajax({
-	        url: contextPath + '/courseModules/updateModule',
-	        data: {
-	            courseModuleId: courseModuleId,  // 傳遞 courseModuleId
-	            courseModuleName: newModuleName  // 傳遞新名稱
-	        },
-	        type: 'POST',
-	        success: function(response) {
-	            if (response) {
-	                window.alert('章節更新成功');
-	                // 更新頁面顯示
-	                $row.find('td:eq(1)').text(newModuleName);
-	                $row.find('td:eq(4)').html(`
+		console.log(courseModuleId);
+
+		if (newModuleName === "") {
+			alert("章節名稱不能為空");
+			return;
+		}
+
+		// 發送 Ajax 請求更新章節名稱
+		$.ajax({
+			url: contextPath + '/courseModules/updateModule',
+			data: {
+				courseModuleId: courseModuleId,  // 傳遞 courseModuleId
+				courseModuleName: newModuleName  // 傳遞新名稱
+			},
+			type: 'POST',
+			success: function (response) {
+				if (response) {
+					window.alert('章節更新成功');
+					// 更新頁面顯示
+					$row.find('td:eq(1)').text(newModuleName);
+					$row.find('td:eq(4)').html(`
 	                    <button class='edit btn btn-primary btn-sm'>編輯</button>
 	                    <button class='delete btn btn-danger btn-sm'>刪除</button>
 	                `);
-	            } else {
-	                window.alert('章節更新失敗');
-	            }
-	        },
-	        error: function(error) {
-	            console.error('Error updating courseModule:', error);
-	        }
-	    });
+				} else {
+					window.alert('章節更新失敗');
+				}
+			},
+			error: function (error) {
+				console.error('Error updating courseModule:', error);
+			}
+		});
 	});
 
 	// 點擊取消編輯按鈕
-	$(document).on('click', '.cancel-edit', function() {
+	$(document).on('click', '.cancel-edit', function () {
 		let $row = $(this).closest('tr');
 		let currentModuleName = $row.find('.editModuleName').val().trim();
 		$row.find('td:eq(1)').text(currentModuleName);
@@ -126,24 +126,29 @@ $(document).ready(function() {
 	});
 
 	//按下刪除按鈕
-	$(document).on('click', '.delete', function() {
+	$(document).on('click', '.delete', function () {
 		var courseModuleId = $(this).data('coursemoduleid');
 		console.log("Selected CourseModule ID for Deletion: " + courseModuleId);
 
-		$.ajax({
-			url: contextPath + '/courseModules/delete?courseModuleId=' + courseModuleId,
-			type: 'GET',
-			success: function(response) {
-				if (response) {
-					window.alert('課程刪除成功');
-					window.location.href = contextPath + '/courseModules?courseId=' + response.courseId;
-				} else {
-					window.alert('課程刪除失敗');
+		let answer = confirm('確認刪除嗎？');
+		if (answer) {
+			$.ajax({
+				url: contextPath + '/courseModules/delete?courseModuleId=' + courseModuleId,
+				type: 'GET',
+				success: function (response) {
+					if (response) {
+						window.alert('課程刪除成功');
+						window.location.href = contextPath + '/courseModules?courseId=' + response.courseId;
+					} else {
+						window.alert('課程刪除失敗');
+					}
+				},
+				error: function (error) {
+					console.error('Error deleting course:', error);
 				}
-			},
-			error: function(error) {
-				console.error('Error deleting course:', error);
-			}
-		});
+			});
+		} else {
+
+		}
 	});
 });
