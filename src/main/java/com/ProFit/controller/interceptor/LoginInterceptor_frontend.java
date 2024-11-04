997295ecc023c377493ef355cdb995d84f8fbeff
Ivan <as82858285@gmail.com>
2024-11-04 14:35:36 +0800

    微調攔截器

--- src/main/java/com/ProFit/controller/interceptor/LoginInterceptor_frontend.java
+++ src/main/java/com/ProFit/controller/interceptor/LoginInterceptor_frontend.java
@@ -2,6 +2,9 @@ package com.ProFit.controller.interceptor;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.ProFit.model.dto.usersDTO.UsersDTO;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -15,10 +18,15 @@ public class LoginInterceptor_frontend implements HandlerInterceptor {
 
 		HttpSession session = request.getSession();
 
-		if (session.getAttribute("CurrentUser") == null) {
+		UsersDTO currentUser = (UsersDTO) session.getAttribute("CurrentUser");
+
+		if (currentUser == null) {
 			response.sendRedirect(request.getContextPath() + "/homepage?login=false");
 			return false;
 		}
+
+		request.setAttribute("CurrentUser", currentUser);
+
 		return true; // 返回 true 繼續請求
 	}
 }
